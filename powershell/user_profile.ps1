51fc5ca36bf55c79e429468678511f15d7919077
charbel-dot <nassehcharbel@gmail.com>
2022-01-14 00:39:50 +0200

    Powershell Setup

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,19 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module Terminal-Icons
+Import-Module PSReadLine
+
+Set-PoshPrompt -Theme fish
+Set-PSReadLineOption -PredictionSource History
+#Set-PSReadLineOption -Colors @{
+	#"Command" = "#7171f7"
+	#"String" = "$([char]0x1b)[38;5;100m"
+	#"ContinuationPrompt" = "#121212"}
+
+# Alias
+Set-Alias vim nvim
+Set-Alias vi nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
